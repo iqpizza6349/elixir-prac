97ae1106b04f0a47898b0e92fb15e45b098a729c
iqpizza6349 <iqpizza6349@gmail.com>
2024-07-15 20:40:52 +0900

    First elixir :droplet::purple_heart:

---
+++ README.md
@@ -0,0 +1,4 @@
+## Elixir Prac.
+
+Elixir 프로그래밍 언어에 대해서 공부하기. <br/>
+공부할 때 참조한 사이트: https://www.tutorialspoint.com/elixir/elixir_basic_syntax.htm
---
+++ hello.ex
@@ -0,0 +1,15 @@
+# 'Hello, World'라는 문자열을 IO 를 통해 출력
+IO.puts "Hello, World!";
+
+# elixir 는 기본적으로 snake_case 를 사용한다.
+cheese_cake = "Cheesy and tasty!";
+
+IO.puts cheese_cake;
+
+# 예약어는 다음과 같다.
+# 참고로 elixir 는 기본적으로 여러 줄 주석을 제공하고 있지 않다고 한다.
+# 대신, @doc 혹은 @moduledoc 을 사용하면 가능하다고 한다.
+# after, and, catch, do, inbits, inlist, nil, else, end, not,
+# or, false, fn, in, rescue, true, when, xor, __MODULE__,
+# __FILE__, __DIR__, __ENV__, __CALLER__
+# 뭔가 파이썬과 JS를 많이 닮은 꼴이다.
