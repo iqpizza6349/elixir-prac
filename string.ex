797cc217ccf37e0d70503d84184fad6aa29d0059
iqpizza6349 <iqpizza6349@gmail.com>
2024-07-19 22:20:50 +0900

    Learn about character in elixir

--- string.ex
+++ string.ex
@@ -29,3 +29,13 @@ IO.puts("foo" =~ format);
 # <> 의 진실.
 # 사실 저건 binary concat 할 때 사용하는 거였다.
 # 하지만 결국 문자열도 binary 배열이니 저렇게 해도 동작하는 것이었다.
+
+# char 는 작은 따옴표, string 은 큰 따옴표로 구분짓는다. (다른 정적 언어들과 동일 혹은 유사)
+IO.puts('Hello');
+IO.puts(is_list('Hello'));
+
+# 그래서 char 에서 문자열로 만들려면 to_string 을 사용하면 되고,
+# string 에서 char 로 만들려면 to_char_list 를 사용하면 된다.
+IO.puts(is_list(to_char_list("Hello")));
+IO.puts(is_binary(to_string(~C"Hello")));
+# 참고로 ~C""인 편을 '' 보다 더 선호한다고 한다.
